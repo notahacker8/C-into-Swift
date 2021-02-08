98276d023b9ebd13191c4fbf3d7e64afe2363e27
King Doggo <77363964+notahacker8@users.noreply.github.com>
2021-02-07 23:46:18 -0700

    Create ViewController.swift

---
+++ App/ViewController.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cfunction()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+
+
+}
