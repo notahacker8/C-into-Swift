5184afe89653b5b6e6af855af162234f9ffa435a
King Doggo <77363964+notahacker8@users.noreply.github.com>
2021-02-07 23:49:26 -0700

    Create App-Bridging-Header.h

---
+++ App/App-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//Make sure that the name of this bridging header is [Project-Name]-Bridging-Header.h
+//If you add a C file into a swift app, it should ask to include a bridging header by default.
+
+#import "c.h"
